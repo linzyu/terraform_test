6b7e53fd3f26c551529d916e6c6c39cda5e90434
linzyu <33408559+linzyu@users.noreply.github.com>
2020-05-13 13:20:05 +0800

    new edit for instance_tenancy
    not using a dedicated use instance_tenancy

--- demo-3/instance.tf
+++ demo-3/instance.tf
@@ -6,7 +6,9 @@ provider "aws" {
 
 #resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
+
+  instance_tenancy = "instance_tenancy"
+#"dedicated" #!! for future all vpcs should be made with instance_tenancy as shared cause dedicated means we have dedicated servers, it costs a lot.
 
   tags = {
     Name = "testing_vpc"
